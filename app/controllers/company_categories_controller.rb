ff37512e165591ab1f1b7b60176c2777c8b1dd02
natail <nataleanfuso@gmail.com>
2017-07-12 21:12:43 -0500

    back to radio buttons. set up form for scale of relevance. need to save to db still

--- app/controllers/company_categories_controller.rb
+++ app/controllers/company_categories_controller.rb
@@ -9,15 +9,20 @@ class CompanyCategoriesController < ApplicationController
     end
 
     def create
+        category_ids = params[:category][:category_id]
+        category_relevances = params[:category][:relevance]
+
+
         @company_category = CompanyCategory.create(
                                                     company_id: current_employee.company.id,
-                                                    category_id: params[:category_id],
-                                                    relevance: 'yes'
+                                                    category_id: [:category_ids][:ids],
+                                                    relevance: params[:category][:relevance]
                                                     )
+
         
         if @company_category.save
             flash[:success] = "Event categories added!"
-            redirect_to '/companycategories'
+            redirect_to '/'
         else redirect_to '/companycategories'
             flash[:warning] = "Please submit again"
         end
--- app/models/company_category.rb
+++ app/models/company_category.rb
@@ -2,5 +2,5 @@ class CompanyCategory < ApplicationRecord
     belongs_to :company
     belongs_to :category
     
-    enum relevance: [:yes, :no]
+    enum relevance: [0, 1, 2, 3, 4, 5]
 end
